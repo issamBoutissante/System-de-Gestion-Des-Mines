2491c66aaec976280dfe02eaead2a9f7f6623df0
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-04-27 11:46:40 +0200

    i have added a class for generating documents word

--- Permis_Recherche.xaml.cs
+++ Permis_Recherche.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace Projet_Mines_Official
 {
-    /// <summary>
-    /// Interaction logic for Permis_Recherche.xaml
-    /// </summary>
     public partial class Permis_Recherche : Window
     {
         ProjetMinesDBContext projetMinesDBContext=new ProjetMinesDBContext();
@@ -28,22 +16,6 @@ namespace Projet_Mines_Official
         void RemplirPointPevot()
         {
             projetMinesDBContext.Point_Pivots.ToList().ForEach(point => Point_Pevot.Items.Add(point.Nom_Point_Pevot));
-        }
-        private void Enregistrer_DemmandeInfo_Click(object sender, RoutedEventArgs e)
-        {
-            //si le permis deja exist on vas le modifier si non on va l'ajouter
-            Permis permis = projetMinesDBContext.Les_Permis.Find(int.Parse(Numero_Demande.Text));
-            if (permis == null)
-            {
-                permis = new Permis();
-                UpdateDemmandeInfo(permis);
-                projetMinesDBContext.Les_Permis.Add(permis);
-                //permis.Etat_Permis = projetMinesDBContext.Etats_Permis.Find(0);
-                //permis.Type_Permis = projetMinesDBContext.Types_Permis.Find(0);
-            }
-            else
-                UpdateDemmandeInfo(permis);
-            projetMinesDBContext.SaveChanges();
         }
         void UpdateDemmandeInfo(Permis permis)
         {
@@ -65,6 +37,7 @@ namespace Projet_Mines_Official
                 Nom_Site = Nom_Site.Text,
                 Effictif = int.Parse(Effective.Text),
             };
+           
         }
         void UpdateAreaInfo(Permis permis)
         {
@@ -83,27 +56,50 @@ namespace Projet_Mines_Official
                 },
             };
         }
-        private void Enregistrer_Titulaire_Info_Click(object sender, RoutedEventArgs e)
+        private void VerifierDonne()
         {
+
+        }
+        private void Enregistrer_DemmandeInfo_Click(object sender, RoutedEventArgs e)
+        {
+            //si le permis deja exist on vas le modifier si non on va l'ajouter
             Permis permis = projetMinesDBContext.Les_Permis.Find(int.Parse(Numero_Demande.Text));
             if (permis == null)
             {
-                MessageBox.Show("Cette Demmande N'existe Pas Essayer de Donne le numero de Demmande");
-                return;
+                permis = new Permis();
+                UpdateDemmandeInfo(permis);
+                projetMinesDBContext.Les_Permis.Add(permis);
+                //permis.Etat_Permis = projetMinesDBContext.Etats_Permis.Find(0);
+                //permis.Type_Permis = projetMinesDBContext.Types_Permis.Find(0);
             }
+            else
+                UpdateDemmandeInfo(permis);
+            projetMinesDBContext.SaveChanges();
+        }
+        private void Enregistrer_Titulaire_Info_Click(object sender, RoutedEventArgs e)
+        {
+            Permis permis = projetMinesDBContext.Les_Permis.Find(int.Parse(Numero_Demande.Text));
             UpdateTitulaireInfo(permis);
             projetMinesDBContext.SaveChanges();
         }
         private void Enregistrer_Area_Info_Click(object sender, RoutedEventArgs e)
         {
             Permis permis = projetMinesDBContext.Les_Permis.Find(int.Parse(Numero_Demande.Text));
-            if (permis == null)
-            {
-                MessageBox.Show("Cette Demmande N'existe Pas Essayer de Donne le numero de Demmande");
-                return;
-            }
             UpdateAreaInfo(permis);
             projetMinesDBContext.SaveChanges();
+        }
+        private void ConvertToEcxel()
+        {
+            //using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+            //{
+            //    if (sfd.ShowDialog() == Di)
+            //    {
+            //        using(XLWorkbook w=new XLWorkbook())
+            //        {
+            //            work
+            //        }
+            //    }
+            //}
         }
     }
 }
