6798e186a14826693115fa5ff1aa23b23b28313e
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-05-26 10:07:24 +0200

    done with recherche digits validation

--- Permis_Recherche.xaml.cs
+++ Permis_Recherche.xaml.cs
@@ -4,6 +4,10 @@ using System.Windows.Controls;
 using System.Linq;
 using Word = Microsoft.Office.Interop.Word;
 using System.Windows.Media;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace Projet_Mines_Official
 {
@@ -197,7 +201,24 @@ namespace Projet_Mines_Official
         #endregion
         private void addChevauchement_Click(object sender, RoutedEventArgs e)
         {
-            Chevauchements.Children.Add(GetChevauchementElement(Convert.ToInt32(ChevauchementCombo.Text))); 
+            int num_Permis=Convert.ToInt32(ChevauchementCombo.Text);
+            bool isExist=this.projetMinesDBContext.Les_Permis.Any(p => p.Num_Permis == num_Permis);
+            if (isExist
+                )
+            {
+                Chevauchements.Children.Add(GetChevauchementElement(num_Permis));
+                return;
+            }
+            ChevauchementCombo.BorderBrush = Brushes.Red;
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    ChevauchementCombo.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFromString("#FF2196F3");
+                });
+            });
+
         }
         private Button GetChevauchementElement(int NumPermis)
         {
@@ -335,7 +356,7 @@ namespace Projet_Mines_Official
                 , dw.documentsContainer, () => { dw.Show(); });
             this.Permis.Etat_PermisId = (int)EtatPermis.Decision;
         }
-
+        
         private void Generer_Lettre_Transmission_Click(object sender, RoutedEventArgs e)
         {
             documentsWord dw = new documentsWord();
@@ -387,6 +408,48 @@ namespace Projet_Mines_Official
                 }
 
                 , dw.documentsContainer, () => { dw.Show(); });
+        }
+
+        private void GetOnlyNumbers_KeyDown(object sender, KeyEventArgs e)
+        {
+            List<Key> keys = new List<Key>()
+            {
+                Key.D0,
+                Key.D1,
+                Key.D2,
+                Key.D3,
+                Key.D4,
+                Key.D5,
+                Key.D6,
+                Key.D7,
+                Key.D8,
+                Key.D9,
+                Key.NumPad0,
+                Key.NumPad1,
+                Key.NumPad2,
+                Key.NumPad3,
+                Key.NumPad4,
+                Key.NumPad5,
+                Key.NumPad6,
+                Key.NumPad7,
+                Key.NumPad8,
+                Key.NumPad9,
+            };
+            
+            if (!keys.Contains(e.Key))
+            {
+                TextBox textBox = sender as TextBox;
+                textBox.BorderBrush = Brushes.Red;
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(2000);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        textBox.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFromString("#FF2196F3");
+                    });
+                });
+                e.Handled = true;
+            }
         }
     }
 }
